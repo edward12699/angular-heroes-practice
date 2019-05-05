61653c9decf35c9a1937c45132e2e00e42d1155e
金晓翔 <>
2019-05-05 17:31:21 +0800

    step1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
+//@Component is a decorator function that specifies the Angular metadata for the component.
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',  // the component's CSS element selector , matches the name of the HTML element that identifies this component within a parent component's template
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ src/app/hero.js
@@ -0,0 +1,21 @@
+"use strict";
+// export class Hero {
+//   id: number;
+//   name: string;
+// }
+exports.__esModule = true;
+var HerosComponent = /** @class */ (function () {
+    function HerosComponent() {
+        this.hero = 'Windstorm';
+        this.c = 1;
+    }
+    HerosComponent.sa = function () {
+        alert(1);
+    };
+    HerosComponent.prototype.b = function () { alert(2); };
+    HerosComponent.a = 1;
+    return HerosComponent;
+}());
+exports.HerosComponent = HerosComponent;
+var herosComponent = new HerosComponent();
+console.log(herosComponent.a);
