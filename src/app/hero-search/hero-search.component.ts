003db962b4563b00c699ef0c8221f871267233e9
金晓翔 <>
2019-05-13 01:18:24 +0800

    finished

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-hero-search',
@@ -6,10 +13,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-search.component.scss']
 })
 export class HeroSearchComponent implements OnInit {
+  heroes$: Observable<Hero[]>;
+  private searchTerms = new Subject<string>(); // 需要这个个临时中间态来做回流优化
+  constructor(private heroService: HeroService) { }
 
-  constructor() { }
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe( // important: 看来这里注册的pipe对于下面的next触发也是有效 ????Z这种绑定关系是因为对象引用的原因还是angular的机制？
+      // wait 300ms after each keystroke before considering the term
+      debounceTime(300),
 
-  ngOnInit() {
+      // ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.heroService.searchHeroes(term)),
+    );
+  }
+  search(term: string): void {
+    this.searchTerms.next(term)
   }
 
 }
